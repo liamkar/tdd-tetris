ba446ae6cdf92556ecb3e620e2d5ab4d2d9307e5
Ville <villekarjala@gmail.com>
2022-04-14 18:51:44 +0300

    pass Rotating 3x3 shape initial orientation -test

---
+++ src/RotatingShape.mjs
@@ -0,0 +1,12 @@
+export class RotatingShape {
+    shape;
+
+    constructor(shape) {
+      this.shape = shape;
+    }
+
+    toString() {
+        return this.shape.replaceAll(" ", "")+"\n";
+    }
+
+  }
