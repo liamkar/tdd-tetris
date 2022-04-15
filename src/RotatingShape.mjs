36fc630cda0322fd05bc58e58c2fd62fa435933b
Ville <villekarjala@gmail.com>
2022-04-15 14:20:01 +0300

    clean before trying refa to generic algorithm

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -25,7 +25,7 @@ export class RotatingShape {
       let height = this.height;
 
           return new RotatingShape(
-            ""+m[height-1][0]   +m[height-2][0]       +m[height-3][0]+"\n"
+            ""+m[height-1][0]   +m[height-2][width-3] +m[height-3][0]+"\n"
               +m[height-1][1]   +m[height-2][width-2] +m[height-3][1]+"\n"            
               +m[height-1][2]   +m[height-2][width-1] +m[height-3][2]
           )    
@@ -35,11 +35,11 @@ export class RotatingShape {
       let m = this.matrix;
       let width = this.width;
       let height = this.height;
-      
+
       return new RotatingShape(
         ""+m[0][width-1]  +m[1][width-1]        +m[2][width-1]+"\n"
-          +m[0][width-2]  +m[height-2][width-2] +m[height-1][width-2]+"\n"            
-          +m[0][0]        +m[1][0]              +m[2][0]
+          +m[0][width-2]  +m[height-2][width-2] +m[2][width-2]+"\n"            
+          +m[0][width-3]  +m[1][width-3]        +m[2][width-3]
       )    
 }
 
