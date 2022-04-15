4f81314e8ee66c4bb501161b9303d03065450016
Ville <villekarjala@gmail.com>
2022-04-15 16:11:53 +0300

    pass Rotating I shape initial orientation -test

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -5,4 +5,13 @@ export class Tetromino {
     static T_SHAPE = new RotatingShape(`.T.
     TTT
     ...`);
+
+    static I_SHAPE = new RotatingShape(
+        `.....
+        .....
+        IIII.
+        .....
+        .....`
+     );
+
   }
